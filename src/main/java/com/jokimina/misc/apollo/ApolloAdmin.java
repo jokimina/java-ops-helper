670586f9c8b74478d48557e3362391d8afd14b67
xiaodong <jokimina@163.com>
2018-12-14 17:32:41 +0800

    Add apollo admin

---
+++ src/main/java/com/jokimina/misc/apollo/ApolloAdmin.java
@@ -0,0 +1,56 @@
+package com.jokimina.misc.apollo;
+
+import com.ctrip.framework.apollo.openapi.client.ApolloOpenApiClient;
+import com.ctrip.framework.apollo.openapi.dto.NamespaceReleaseDTO;
+import com.ctrip.framework.apollo.openapi.dto.OpenNamespaceDTO;
+import com.google.common.base.Strings;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author xiaodong
+ * 12/14/2018
+ */
+
+public class ApolloAdmin {
+    public static void main(String[] args) {
+        String url = System.getProperty("url");
+        String token = System.getProperty("token");
+        final List<String> apps = Arrays.asList("sc-user");
+        final String env = "PRO";
+        final String sClusterName = "indonesia";
+        final String dClusterName = "singapore";
+        ApolloOpenApiClient client = ApolloOpenApiClient.newBuilder().withPortalUrl(url).withToken(token).build();
+        apps.stream().forEach(appId -> {
+            syncCluster(client, appId, env, sClusterName, dClusterName);
+        });
+    }
+
+    static void syncCluster(ApolloOpenApiClient client, String appId, String env, String sClusterName, String dClusterName) {
+        List<OpenNamespaceDTO> sNamespaces = client.getNamespaces(appId, env, sClusterName);
+        sNamespaces.stream().forEach(sNamespace -> {
+            OpenNamespaceDTO dNamespace = client.getNamespace(appId, env, dClusterName, sNamespace.getNamespaceName());
+            System.out.println(String.format("Sync env %s app %s cluster %s namespace %s -> Sync env %s app %s cluster %s namespace %s", env, appId, sClusterName, sNamespace.getNamespaceName(), env, appId, dClusterName, dNamespace.getNamespaceName()));
+            sNamespace.getItems().stream().forEach(item -> {
+                try {
+                    System.out.println(item.getKey());
+                    if (!Strings.isNullOrEmpty(item.getKey()) && !Strings.isNullOrEmpty(item.getValue())) {
+                        try {
+                            client.createItem(appId, env, dClusterName, dNamespace.getNamespaceName(), item);
+                        } catch (Exception e) {
+                            client.updateItem(appId, env, dClusterName, dNamespace.getNamespaceName(), item);
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+            NamespaceReleaseDTO namespaceReleaseDTO = new NamespaceReleaseDTO();
+            namespaceReleaseDTO.setReleaseTitle("Release by java openapi");
+            namespaceReleaseDTO.setReleaseComment("Initial for " + dClusterName);
+            namespaceReleaseDTO.setReleasedBy("apollo");
+            client.publishNamespace(appId, env, dClusterName, dNamespace.getNamespaceName(), namespaceReleaseDTO);
+        });
+    }
+}
